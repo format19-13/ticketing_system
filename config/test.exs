f62e1247b4eee2958f3431b399cefc4b4a22d3da
Marce <marcegithub@gmail.com>
2017-10-25 02:39:10 -0200

    change database passworsd

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,6 @@ config :logger, level: :warn
 config :ticketing_system, TicketingSystem.Repo,
 adapter: Ecto.Adapters.MySQL,
 username: "root",
-password: "patricia",
 database: "ticketing_system_test",
 hostname: "localhost",
 pool: Ecto.Adapters.SQL.Sandbox
